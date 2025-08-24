e0a86a0e7c03ec0813c57dfe410bfd3595821bf5
Lucas Andrade <eudevlucas@gmail.com>
2025-08-24 18:04:32 -0300

    fix: global middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\AdminUserMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            AdminUserMiddleware::class
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
