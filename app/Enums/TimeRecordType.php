1e41952fe18b0e0871d43db5a1b84ee9bab8e43c
Lucas Andrade <eudevlucas@gmail.com>
2025-08-25 23:19:44 -0300

    fix: adjust icon colors

--- app/Enums/TimeRecordType.php
+++ app/Enums/TimeRecordType.php
@@ -18,10 +18,10 @@ enum TimeRecordType: string
     public function icon(): string
     {
         return match($this) {
-            self::IN => '<svg class="w-5 h-5 text-red-500 inline" stroke="currentColor" viewBox="0 0 24 24">
+            self::IN => '<svg class="w-5 h-5 text-green-500 inline" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v12m0 0l-4-4m4 4l4-4" />
                                     </svg>',
-            self::OUT => '<svg class="w-5 h-5 text-green-500 inline" stroke="currentColor" viewBox="0 0 24 24">
+            self::OUT => '<svg class="w-5 h-5 text-red-500 inline" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21V9m0 0l-4 4m4-4l4 4" />
                                     </svg>'
         };
